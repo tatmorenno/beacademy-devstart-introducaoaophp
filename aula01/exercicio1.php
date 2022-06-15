7c48ae173d405c88afcb02c9907fdfe364572c4b
Tatiana Moreno <tatiana.emilia.morenno@gmail.com>
2022-06-15 16:07:50 -0300

    Exercício 1

---
+++ aula01/exercicio1.php
@@ -0,0 +1,37 @@
+<form action="" method="post">
+  <input name="valor1" placeholder="Digite o primeiro valor">
+  <input name="valor2" placeholder="Digite o segundo valor">
+
+  <button name="soma">Somar</button>
+  <button name="subtracao">Subtrair</button>
+  <button name="multiplicacao">Multiplicar</button>
+  <button name="divisao">Dividir</button>
+</form>
+
+<?php
+  if ($_POST) {
+    
+    if (isset($_POST['soma'])) {
+      echo "Olá, o primeiro valor foi: ".$_POST['valor1'];
+      echo "</br>Segundo valor: ".$_POST['valor2'];
+      echo "<br/><br/><strong>A soma dos dois números é: " . $_POST['valor1'] + $_POST['valor2']."</strong>";
+    }
+
+    if (isset($_POST['subtracao'])) {
+      echo "Olá, o primeiro valor foi: ".$_POST['valor1'];
+      echo "</br>Segundo valor: ".$_POST['valor2'];
+      echo "<br/><br/><strong>A subtração dos dois números é: " . $_POST['valor1'] - $_POST['valor2']."</strong>";
+    }
+
+    if (isset($_POST['multiplicacao'])) {
+      echo "Olá, o primeiro valor foi: ".$_POST['valor1'];
+      echo "</br>Segundo valor: ".$_POST['valor2'];
+      echo "<br/><br/><strong>A multiplicação dos dois números é: " . $_POST['valor1'] * $_POST['valor2']."</strong>";
+    }
+
+    if (isset($_POST['divisao'])) {
+      echo "Olá, o primeiro valor foi: ".$_POST['valor1'];
+      echo "</br>Segundo valor: ".$_POST['valor2'];
+      echo "<br/><br/><strong>A divisão dos dois números é: " . $_POST['valor1'] / $_POST['valor2']."</strong>";
+    }
+  }
