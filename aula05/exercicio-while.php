5bb580db450b0620bf24950c17257ab0520974c8
Tatiana Moreno <tatiana.emilia.morenno@gmail.com>
2022-06-16 17:24:47 -0300

    Inserindo o select do ano

---
+++ aula05/exercicio-while.php
@@ -0,0 +1,13 @@
+<select>
+  <option selected>-- Selecione o ano --</option>
+  
+  <?php
+
+    $ano = 2022;
+
+    while ($ano > 1899) {
+      echo "<option>{$ano}</option>";
+      $ano--;
+    }
+  ?>
+</select>
