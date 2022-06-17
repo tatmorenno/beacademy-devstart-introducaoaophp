7d426917fa3871abbd54de07751b3cdb93a468e3
Tatiana Moreno <tatiana.emilia.morenno@gmail.com>
2022-06-17 01:46:55 -0300

    Adicionando array bidimensional de cantores

---
+++ aula07/cantores.php
@@ -0,0 +1,14 @@
+<?php
+
+$cantores = [
+  [
+  'Sia',
+  '3',
+  ],
+  ['Britney Spears', '7'],
+  ['Christina Aguilera', '2'],
+  ['Rihanna', '5'],
+  ['Beyonce', '5']
+];
+
+echo $cantores[2][0];
