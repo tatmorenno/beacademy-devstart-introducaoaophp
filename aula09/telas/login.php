75b0549756e0f14f3a6a9526a18f0326faddd0a7
Tatiana Moreno <tatiana.emilia.morenno@gmail.com>
2022-06-18 01:50:05 -0300

    Bootstrap na página de login

---
+++ aula09/telas/login.php
@@ -0,0 +1,7 @@
+<h1>Login</h1>
+<hr>
+<form action="">
+  <input class="form-control mb-3" type="text" placeholder="email">
+  <input class="form-control mb-3" type="password" placeholder="senha">
+  <button class="btn btn-primary">Enviar</button>
+</form>
