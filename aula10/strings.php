f533bf517880440b2aad724ce91a29b84b1d4a6d
Tatiana Moreno <tatiana.emilia.morenno@gmail.com>
2022-06-18 01:43:42 -0300

    Funções para letras MAIÚSCULAS e minúsculas

---
+++ aula10/strings.php
@@ -0,0 +1,9 @@
+ <?php
+
+ $nome = 'tatiana';
+ $sobrenome = 'MORENO';
+ $nomeCompleto = 'mariquinha da silva';
+
+ echo "Nome: " . ucfirst($nome) . PHP_EOL;
+ echo "Sobrenome: " . ucfirst(strtolower($sobrenome)) . PHP_EOL;
+ echo "Nome: " . ucwords($nomeCompleto) . PHP_EOL;
