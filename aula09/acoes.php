0aa94896b13ea88eb24e1fd5cd934bcca11dce23
Tatiana Moreno <tatiana.emilia.morenno@gmail.com>
2022-06-18 01:55:59 -0300

    Criando métodos para redirecionamento das páginas

---
+++ aula09/acoes.php
@@ -0,0 +1,25 @@
+<?php
+
+function home() {
+  include 'telas/home.php';
+}
+
+function login() {
+  include 'telas/login.php';
+}
+
+function cadastro() {
+  include 'telas/cadastro.php';
+}
+
+function listar() {
+  include 'telas/listar.php';
+}
+
+function relatorio() {
+  include 'telas/relatorio.php';
+}
+
+function erro404() {
+  include 'telas/404.php';
+}
