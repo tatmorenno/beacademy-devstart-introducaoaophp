e628ad1e79b1097bcf7e73a6c0fa41b7e793827f
Tatiana Moreno <tatiana.emilia.morenno@gmail.com>
2022-06-18 01:53:35 -0300

    Criando página de cadastro

---
+++ aula09/telas/cadastro.php
@@ -0,0 +1,8 @@
+<h1>Cadastro</h1>
+<hr>
+<form action="">
+  <input type="text" placeholder="Nome">
+  <input type="text" placeholder="Email">
+  <input type="password" placeholder="Senha">
+  <button>Enviar</button>
+</form>
